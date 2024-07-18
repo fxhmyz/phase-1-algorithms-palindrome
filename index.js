3520a99517ebeac82285643d8f451ce740dd5ac5
fxhmyz <alifahmy99111@gmail.com>
2024-07-18 18:46:07 +0300

    first initial commit

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
 }
+function isPalindrome(str) {
+  // Convert to lowercase and remove non-alphabetic characters
+  str = str.toLowerCase().replace(/[^a-z]/g, '');
+  
+  let left = 0;
+  let right = str.length - 1;
+  
+  while (left < right) {
+      if (str[left] !== str[right]) {
+          return false;
+      }
+      left++;
+      right--;
+  }
+  
+  return true;
+}
+
+// Test cases
+console.log(isPalindrome("madam")); // true
+console.log(isPalindrome("racecar")); // true
+console.log(isPalindrome("hello")); // false
+console.log(isPalindrome("Aibohphobia")); // false
 
 /* 
   Add your pseudocode here
